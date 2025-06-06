15dbbbaa8d4e18ea0a75293dc81a12d7be1ae598
nunhes <nunhes@gmail.com>
2025-06-06 13:31:07 +0200

    cambios cart

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,143 @@
+@extends('layouts.main')
+
+@section('content')
+
+<main class="mt-0">
+    <!-- cards -->
+    <div class="relative px-6 pt-16 pb-20 bg-gray-50 lg:px-8 lg:pt-24 lg:pb-28">
+        <div class="absolute inset-0">
+            <div class="bg-white h-1/3 sm:h-2/3"></div>
+        </div>
+        <div class="relative mx-auto max-w-7xl">
+            <div class="text-center">
+                <div class="w-32 h-1 mx-auto mb-6 bg-pink-500"></div>
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Terms &amp; Conditons</h2>
+                <p class="max-w-2xl mx-auto mt-3 text-xl text-gray-500 sm:mt-4">
+                    This is your life and it's ending one minute @ a time...</p>
+            </div>
+            <div class="grid max-w-lg gap-5 mx-auto mt-12 lg:max-w-none lg:grid-cols-3">
+
+                <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                    <div class="flex-shrink-0">
+                        <img class="object-cover w-full h-48" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1679&amp;q=80" alt="">
+                    </div>
+                    <div class="flex flex-col justify-between flex-1 p-6 bg-white">
+                        <div class="flex-1">
+                            <p class="text-sm font-medium text-indigo-600">
+                                <a href="#" class="hover:underline">Article</a>
+                            </p>
+                            <a href="#" class="block mt-2">
+                                <p class="text-xl font-semibold text-gray-900">Boost your conversion rate</p>
+                                <p class="mt-3 text-base text-gray-500">Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit.
+                                    Architecto accusantium praesentium eius, <a class="text-blue-600 after:content-['_↗'] ..." href="https://www.
+                                        apple.com/pro-display-xdr/" target="_blank"> ut atque fuga culpa</a>, similique sequi cum
+                                    eos quis dolorum.</p>
+                            </a>
+                        </div>
+                        <div class="flex items-center mt-6">
+                            <div class="flex-shrink-0">
+                                <a href="#">
+                                    <span class="sr-only">Roel Aufderehar</span>
+                                    <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                </a>
+                            </div>
+                            <div class="ml-3">
+                                <p class="text-sm font-medium text-gray-900">
+                                    <a href="#" class="hover:underline">Roel Aufderehar</a>
+                                </p>
+                                <div class="flex space-x-1 text-sm text-gray-500">
+                                    <time datetime="2020-03-16">Mar 16, 2020</time>
+                                    <span aria-hidden="true">·</span>
+                                    <span>6 min read</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                    <div class="flex-shrink-0">
+                        <img class="object-cover w-full h-48" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1679&amp;q=80" alt="">
+                    </div>
+                    <div class="flex flex-col justify-between flex-1 p-6 bg-white">
+                        <div class="flex-1">
+                            <p class="text-sm font-medium text-indigo-600">
+                                <a href="#" class="hover:underline">Video</a>
+                            </p>
+                            <a href="#" class="block mt-2">
+                                <p class="text-xl font-semibold text-gray-900">How to use search engine optimization to
+                                    drive sales</p>
+                                <p class="mt-3 text-base text-gray-500">Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Velit
+                                    facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut
+                                    inventore optio animi.,
+                                    tempore temporibus quo laudantium.</p>
+                            </a>
+                        </div>
+                        <div class="flex items-center mt-6">
+                            <div class="flex-shrink-0">
+                                <a href="#">
+                                    <span class="sr-only">Brenna Goyette</span>
+                                    <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                </a>
+                            </div>
+                            <div class="ml-3">
+                                <p class="text-sm font-medium text-gray-900">
+                                    <a href="#" class="hover:underline">Brenna Goyette</a>
+                                </p>
+                                <div class="flex space-x-1 text-sm text-gray-500">
+                                    <time datetime="2020-03-10">Mar 10, 2020</time>
+                                    <span aria-hidden="true">·</span>
+                                    <span>4 min read</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                    <div class="flex-shrink-0">
+                        <img class="object-cover w-full h-48" src="https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1679&amp;q=80" alt="">
+                    </div>
+                    <div class="flex flex-col justify-between flex-1 p-6 bg-white">
+                        <div class="flex-1">
+                            <p class="text-sm font-medium text-indigo-600">
+                                <a href="#" class="hover:underline">Case Study</a>
+                            </p>
+                            <a href="#" class="block mt-2">
+                                <p class="text-xl font-semibold text-gray-900">Improve your customer experience</p>
+                                <p class="mt-3 text-base text-gray-500">Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Sint
+                                    harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi
+                                    cumque corporis
+                                    perferendis hic.</p>
+                            </a>
+                        </div>
+                        <div class="flex items-center mt-6">
+                            <div class="flex-shrink-0">
+                                <a href="#">
+                                    <span class="sr-only">Daniela Metz</span>
+                                    <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                                </a>
+                            </div>
+                            <div class="ml-3">
+                                <p class="text-sm font-medium text-gray-900">
+                                    <a href="#" class="hover:underline">Daniela Metz</a>
+                                </p>
+                                <div class="flex space-x-1 text-sm text-gray-500">
+                                    <time datetime="2020-02-12">Feb 12, 2020</time>
+                                    <span aria-hidden="true">·</span>
+                                    <span>11 min read</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</main>
+
+@endsection
